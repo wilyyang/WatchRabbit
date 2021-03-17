be4c45cab818f6c5a5e4cdd41e6e1bf09846b6fc
wilyyang <ehdrnr1178@gmail.com>
2021-03-17 15:52:03 +0900

    UI added Evaluation

---
+++ app/src/main/java/wily/apps/watchrabbit/EvaluationHabbitActivity.java
@@ -0,0 +1,14 @@
+package wily.apps.watchrabbit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EvaluationHabbitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluation_habbit);
+    }
+}
